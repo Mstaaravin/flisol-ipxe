3ebc7e0a5ee5be3b827111aee08bdf9a9abce287
Rino Rondan <villadalmine@gmail.com>
2014-04-20 11:35:23 -0300

    Mas scripts para debmirror

---
+++ debmirror/fedora.sh
@@ -0,0 +1,2 @@
+rsync -aqvzH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/i386/os/ /home/fedora/20/os/i386/
+rsync -aqzvH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/x86_64/os/ /home/fedora/20/os/x86_64/
