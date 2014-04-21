656b4e3a3d8aa3b0fd0c352dab3dc5d34c983cb1
Rino Rondan <villadalmine@gmail.com>
2014-04-20 21:25:04 -0300

    Se modifico el path destino

--- debmirror/fedora.sh
+++ debmirror/fedora.sh
@@ -1,2 +1,2 @@
-rsync -aqvzH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/i386/os/ /home/fedora/20/os/i386/
-rsync -aqzvH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/x86_64/os/ /home/fedora/20/os/x86_64/
+rsync -aqvzH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/i386/os/ /home/mirrors/fedora/20/os/i386/
+rsync -aqzvH --delete rsync://mirror.nexicom.net/Fedora/releases/20/Fedora/x86_64/os/ /home/mirrors/fedora/20/os/x86_64/
